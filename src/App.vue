19f80ccc4dfd47c99a3a848b036b61aff7603019
ChanNimol <160984970+NimolChan@users.noreply.github.com>
2024-10-28 14:07:10 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -31,21 +31,21 @@ export default{
   data(){
     return{
       products:[
-        {img:'../image/buger.png', title:"Cake & Mile",item:14+"items",bgColor:'#F2FCE4'},
-        {img:'../image/peach.png', title:"Peach",item:17+"items", bgColor:'#FFFCEB'},
-        {img:'../image/kiwi.png', title:"Oganic Kiwi",item:68+"items", bgColor:'#ECFFEC'},
-        {img:'../image/apple.png', title:"Red Apple",item:34+"items", bgColor:'#FEEFEA'},
-        {img:'../image/snac.png', title:"Snack",item:25+"items", bgColor:'#FFF3EB'},
-        {img:'../image/plum.png', title:"Black plum",item:10+"items", bgColor:'#FFF3FF'},
-        {img:'../image/vegatable.png', title:"Vegetable",item:65+"items", bgColor:'#F2FCE4'},
-        {img:'../image/headphone.png', title:"Headphone",item:33+"items", bgColor:'#FFFCEB'},
-        {img:'../image/sack.png', title:"Cake & Mile",item:54+"items", bgColor:'#F2FCE4'},
-        {img:'../image/orange.png', title:"Orange",item:63+"items", bgColor:'#FFF3FF'},
+        {img:'../image/buger.png', title:"Cake & Mile",item:14 +"items",bgColor:'#F2FCE4'},
+        {img:'../image/peach.png', title:"Peach",item:17 +"items", bgColor:'#FFFCEB'},
+        {img:'../image/kiwi.png', title:"Oganic Kiwi",item:68 +"items", bgColor:'#ECFFEC'},
+        {img:'../image/apple.png', title:"Red Apple",item:34 +"items", bgColor:'#FEEFEA'},
+        {img:'../image/snac.png', title:"Snack",item:25 +"items", bgColor:'#FFF3EB'},
+        {img:'../image/plum.png', title:"Black plum",item:10 +"items", bgColor:'#FFF3FF'},
+        {img:'../image/vegatable.png', title:"Vegetable",item:65 +"items", bgColor:'#F2FCE4'},
+        {img:'../image/headphone.png', title:"Headphone",item:33 +"items", bgColor:'#FFFCEB'},
+        {img:'../image/sack.png', title:"Cake & Mile",item:54 +"items", bgColor:'#F2FCE4'},
+        {img:'../image/orange.png', title:"Orange",item:63 +"items", bgColor:'#FFF3FF'},
       ],
       Promotions:[
-        {bgImage:'../image/background1.jpg',bgColorSecond:'#F0E8D5',TittlePromotion:"Everyday Fresh & Clean with Our Products"},
-        {bgImage:'../image/background2.png',bgColorSecond:'#F3E8E8',TittlePromotion:"Make your Breakfast Healthy and Easy"},
-        {bgImage:'../image/background3.jpg',bgColorSecond:'#E7EAF3',TittlePromotion:"The best Organic Products Online"},
+        {bgImage:'../image/bg1.jpg',bgColorSecond:'#F0E8D5',TittlePromotion:"Everyday Fresh & Clean with Our Products"},
+        {bgImage:'../image/bg2.png',bgColorSecond:'#F3E8E8',TittlePromotion:"Make your Breakfast Healthy and Easy"},
+        {bgImage:'../image/bg3.jpg',bgColorSecond:'#E7EAF3',TittlePromotion:"The best Organic Products Online"},
       ]
     };
   },
