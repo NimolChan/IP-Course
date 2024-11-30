6efc5f9a436855cdaa4d7bee97d01dd888e318a2
nimol <channimol150903@gmail.com>
2024-11-30 14:35:56 +0700

    First TP4

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-
-const router = createRouter({
+import HomeView from '@/views/HomeView.vue'
+ 
+const router =createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-  ]
-})
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+      },
+      // {
+      //   path: "/category/:categoryId",
+      //   name: "category",
+      //   component: () => import("../views/CategoryView.vue"),
+      // },
+      // {
+      //   path: "/products/:productId",
+      //   name: "product",
+      //   component: () => import("../views/ProductView.vue"),
+      // },
+    ],
 
-export default router
- 
+
+});
+export default router;
