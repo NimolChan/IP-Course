9b65a86504781964187745862e1362e465d19f58
NimolChan <channimol150903@gmail.com>
2025-04-03 15:44:32 +0700

    TP7

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -1,8 +1,9 @@
 <?php
-
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Order;
+use App\Observers\ModelActivityObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,9 +17,9 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
-     */
+    */
     public function boot(): void
     {
-        //
+        Order::observe(ModelActivityObserver::class);
     }
 }
---
+++ laravel/database/migrations/2025_04_03_080501_create_payment.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('order_id')->unsigned();
+            $table->bigInteger('customer_id')->unsigned();
+            $table->decimal('amount', 10, 2);
+            $table->string('payment_method')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment');
+    }
+};
