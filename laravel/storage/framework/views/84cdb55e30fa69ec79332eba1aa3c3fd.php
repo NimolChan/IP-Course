17b5b9de6c039b4f3b5bd2096270767d4956b8bb
ChanNimol <channimol150903@gmail.com>
2025-05-16 14:18:02 +0700

    update

---
+++ laravel/storage/framework/views/84cdb55e30fa69ec79332eba1aa3c3fd.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+</head>
+
+<body class="d-flex justify-content-center align-items-center vh-100 text-center">
+    <h1 style="font-size: 150px; display: flex;">Welcome To E-Commerce Backend Server</h1>
+</body>
+
+</html><?php /**PATH /var/www/resources/views/welcome.blade.php ENDPATH**/ ?>
