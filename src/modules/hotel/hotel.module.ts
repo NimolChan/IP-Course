903d70de8427852fe56b26a92ece62d2d752f3b7
ChanNimol <channimol150903@gmail.com>
2025-06-12 08:58:41 +0700

    TP12

---
+++ src/modules/hotel/hotel.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { HotelResolver } from './hotel.resolver';
+
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [HotelResolver],
+})
+export class HotelModule {}
